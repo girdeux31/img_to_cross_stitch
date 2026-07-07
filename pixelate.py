f9c48eb85cfd28d6de431741f61e319ce4ece14e
girdeux31 <mesado31@gmail.com>
2026-07-07 12:29:24 +0200

    ref:main program pixelate.py

--- pixelate.py
+++ pixelate.py
@@ -1,9 +1,9 @@
 import sys
 import cairosvg
-from PIL import Image
-from DMC import DMC
-from SVG import SVG
 from pathlib import Path
+
+from pattern import Pattern
+from legend import Legend
 
 
 def svg_to_png(svg_file: Path):
@@ -18,18 +18,9 @@ def svg_to_png(svg_file: Path):
     with open(png_file, 'wb') as f:
         f.write(png_bytes)
 
-def get_neighbours(pos, matrix):
-    rows = len(matrix)
-    cols = len(matrix[0]) if rows else 0
-    width = 1
-    for i in range(max(0, pos[0] - width), min(rows, pos[0] + width + 1)):
-        for j in range(max(0, pos[1] - width), min(cols, pos[1] + width + 1)):
-            if not (i == pos[0] and j == pos[1]):
-                yield matrix[i][j]
-
 if __name__ == '__main__':
 
-    # process arguments
+    # Process user arguments
 
     # if(len(sys.argv)<3):
     #     print("function requires an input filename, number of colors, stitch count and mode")
@@ -37,124 +28,32 @@ if __name__ == '__main__':
 
     # input_file = Path(sys.argv[1])       # input file name, has to be a jpg
     # n_colors = int(sys.argv[2])    # number of colors to use in the pattern
-    # n_squares = int(sys.argv[3])   # stitch count, number of stitches in x axis
+    # n_stitches_per_row = int(sys.argv[3])   # stitch count, number of stitches in x axis
 
     # Just for debugging
     input_file = Path('examples/bird.jpg')
     n_colors = 3
-    n_squares = 50
+    n_stitches_per_row = 50
 
     if not input_file.exists():
         raise FileNotFoundError(f'File \'{input_file}\' not found')
 
-    # init svg objects
-        
-    svg_rgb = SVG(color=True)
-    svg_bw = SVG(color=False)
-    svg_legend = SVG(color=True)
-
-    # read and resize image
-
-    img = Image.open(input_file).convert('RGB')  # make sure to read it in RGM mode
-    new_width  = 1000
-    pixel_size = int(new_width / int(n_squares))
-    new_height = int(new_width * img.size[1] / img.size[0])
-    img = img.resize((new_width, new_height), Image.NEAREST)
-
-    # get 2d list with DMC colors (each element is a RGB tuple)
-
-    dmc = DMC()
-    dmc_spaced = [
-        [
-            dmc.get_most_similar_rgb_by_rgb(img.getpixel((x, y))) 
-            for x in range(0, img.size[0], pixel_size)
-        ]
-        for y in range(0, img.size[1], pixel_size)
-    ]
-
-    # create a new image with previous RGB colors
-
-    dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced))) #h, w
-    dmc_image.putdata([value for row in dmc_spaced for value in row])
-
-    # quantize the image with the required number of colors
-
-    dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors=n_colors)
-    x_count = dmc_image.size[0]
-    y_count = dmc_image.size[1]
-    svg_pattern = [[dmc_image.getpixel((x, y)) for x in range(x_count)] for y in range(y_count)]
-
-    # get image palette (list of colors)
-
-    palette_list = []
-    palette = dmc_image.getpalette()
-    for idx in range(n_colors):
-        rgb = tuple(palette[idx*3:idx*3+3])
-        code = dmc.get_most_similar_code_by_rgb(rgb, corrected=True)
-        name = dmc.get_color_name_by_code(code)
-        palette_list.append(
-            {
-                'rgb': rgb,
-                'code': code,
-                'name': name,
-            }
-        )
-
-    # remove isolated pixels with a local color average
-
-    for x in range(0, x_count):
-        for y in range(0, y_count):
-            gen = get_neighbours([y, x], svg_pattern)
-            neighbours = []
-            for n in gen:
-                neighbours += [n]
-            if svg_pattern[y][x] not in neighbours:
-                mode = max(neighbours, key=neighbours.count)
-                svg_pattern[y][x] = mode
-
-    # create images:
-    #  - B&W wo symbols
-    #  - RGB w symbols
-    #  - RGB wo symbols
-    # then, extra features are added:
-    #  - midpoint arrows
-    #  - major and minor grids
-    #  - symbols
-
-    svg_cell_size = 10
-    width = x_count * svg_cell_size
-    height = y_count * svg_cell_size
-    svg_rgb.add_header(width, height)
-    svg_rgb.add_arrows(svg_cell_size, width, height)
-    svg_bw.add_header(width, height)
-    svg_bw.add_arrows(svg_cell_size, width, height)
-    x = y = svg_cell_size # to allow drawing of midpoint arrows
-    for row in svg_pattern:
-        for c_idx in row:
-            svg_rgb.add_color(palette_list, c_idx, x, y, svg_cell_size)
-            svg_rgb.add_symbol(c_idx, x, y, svg_cell_size)
-            svg_bw.add_color(palette_list, c_idx, x, y, svg_cell_size)
-            svg_bw.add_symbol(c_idx, x, y, svg_cell_size)
-            x += svg_cell_size
-        y += svg_cell_size
-        x = svg_cell_size
-    svg_bw.add_gridlines(svg_cell_size, width, height)
-    svg_rgb.add_gridlines(svg_cell_size, width, height)
-
-    # generate the legend image
-
-    size = 40
-    svg_legend.add_header(size*13, size*n_colors)
-    y = 0
-    for idx in range(n_colors):
-        svg_legend.add_legend(y, size, idx, palette_list[idx])
-        y += size
-
-    # save all images
+    # Generate file paths
 
     out_path = input_file.parent
     out_name = input_file.stem
-    svg_rgb.save(out_path / f'{out_name}_rgb.svg')
-    svg_bw.save(out_path / f'{out_name}_bw.svg')
-    svg_legend.save(out_path / f'{out_name}_legend.svg')
-    svg_to_png(out_path / f'{out_name}_rgb.svg')
+    svg_pattern_file = out_path / f'{out_name}_pattern.svg'
+    svg_legend_file = out_path / f'{out_name}_legend.svg'
+
+    # Generate pattern svg
+
+    pattern = Pattern(color=True, symbols=True)
+    pattern.process_image(input_file, n_colors, n_stitches_per_row)
+    pattern.generate()
+    pattern.save(svg_pattern_file)
+
+    # Generate legend svg
+
+    legend = Legend(color=True, symbols=True)
+    legend.generate(pattern.get_palette())
+    legend.save(svg_legend_file)
---
+++ svg.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+
+class SVG:
+    
+    def __init__(self):
+        """Init object"""
+        self.xml = ''
+
+    def _write_xml_line(self, xml: str, indent: int=0) -> None:
+        """Add xml code as string"""
+        self.xml += indent*'\t' + xml + '\n'
+
+    def add_xml_rect(
+        self,
+        x: int | float,
+        y: int | float,
+        width: int | float,
+        height: int | float,
+        style_dict: dict[str, str]
+    ) -> None:
+        """Add xml rect tag"""
+        xml_code = f'<rect x="{x}" y="{y}" width="{width}" height="{height}" style="'
+        for style_arg, style_value in style_dict.items():
+            xml_code += f'{style_arg}:{style_value};'
+        xml_code += '"/>'
+        self._write_xml_line(xml_code, indent=1)
+
+    def add_xml_header(
+        self,
+        width: int | float,
+        height: int | float,
+        style_dict: dict[str, dict[str, str]],
+    ) -> None:
+        """Add xml svg tag"""
+        xml_code = f'<svg xmlns="http://www.w3.org/2000/svg" width="{width}" height="{height}" style="'
+        for style_arg, style_value in style_dict.items():
+            xml_code += f'{style_arg}:{style_value};'
+        xml_code += '">'
+        self._write_xml_line(xml_code)
+
+    def add_xml_style(self, class_dict: dict[str, dict[str, str]]) -> None:
+        """Add xml style tag"""
+        xml_code = '<style>'
+        for class_name, class_style in class_dict.items():
+            xml_code += f'.{class_name}{{'
+            for style_arg, style_value in class_style.items():
+                xml_code += f'{style_arg}:{style_value};'
+            xml_code += '}'
+        xml_code += '</style>'
+        self._write_xml_line(xml_code, indent=1)
+
+    def add_xml_path(self, code: str, style_dict: dict[str, str], class_name: str='') -> None:
+        """Add xml path tag"""
+        xml_code = '<path '
+        if class_name:
+            xml_code += f'class="{class_name}" '
+        xml_code += f'd="{code}"'
+        for style_arg, style_value in style_dict.items():
+            xml_code += f' {style_arg}="{style_value}"'
+        xml_code += '/>'
+        self._write_xml_line(xml_code, indent=1)
+
+    def add_xml_text(self, x: int | float, y: int | float, style_dict: dict[str, str], text: str) -> None:
+        """Add xml text tag"""
+        xml_code = f'<text x="{x}" y="{y}"'
+        for style_arg, style_value in style_dict.items():
+            xml_code += f' {style_arg}="{style_value}"'
+        xml_code += f'>{text}</text>'
+        self._write_xml_line(xml_code, indent=1)
+
+    def add_xml_line(self, x1: int | float, y1: int | float, x2: int | float, y2: int | float, style_dict: dict[str, str]) -> None:
+        """Add xml line tag"""
+        xml_code = f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" style="'
+        for style_arg, style_value in style_dict.items():
+            xml_code += f'{style_arg}:{style_value};'
+        xml_code += '"/>'
+        self._write_xml_line(xml_code, indent=1)
+
+    def save(self, filename: Path) -> None:
+        """Save as svg file"""
+        self._write_xml_line('</svg>')
+        f = open(filename,'w')
+        f.write(self.xml)
+        f.close()
